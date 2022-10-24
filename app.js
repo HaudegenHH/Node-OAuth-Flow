bc040e284b85db6af487f88365c395d80ae93458
HaudegenHH <einsundeins79@gmx.de>
2022-10-24 22:12:08 +0200

    installed passport-google-oauth package

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const authRoutes = require('./routes/auth-routes');
 
 // set up view engine
 app.set('view engine', 'ejs');
 
+// set up auth routes
+app.use('/auth', authRoutes);
+
+// home route
 app.get('/', (req, res) => {
   res.render('home');
 });
